a4840806096adbe857ae31e2c1e6f904dfed214d
yosoyafa <carlosafanador97@gmail.com>
2025-02-24 19:19:46 -0500

    Refactor ESLint configuration, update asset paths, and add EAS configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,34 @@
 module.exports = {
-  extends: ["expo", "prettier"],
-  plugins: ["prettier"],
+  extends: ['expo', 'prettier'],
+  plugins: ['prettier'],
   rules: {
-    "import/order": 0,
-    "react-native/no-inline-styles": 0,
-    "import/namespace": 0,
-    "no-duplicate-imports": "error",
-    "prettier/prettier": "error",
+    'import/order': 0,
+    'react-native/no-inline-styles': 0,
+    'import/namespace': 0,
+    'no-duplicate-imports': 'error',
+    'prettier/prettier': 'error'
   },
   env: {
-    node: true,
+    node: true
   },
   ignorePatterns: [
-    "node_modules/**",
-    "package.json",
-    "yarn.lock",
-    "ios/**",
-    "android/**",
-    "assets/**",
-    ".vscode",
-    ".expo-shared",
-    ".prettierrc",
-    "eslintrc.js",
+    'node_modules/**',
+    'package.json',
+    'yarn.lock',
+    'ios/**',
+    'android/**',
+    'assets/**',
+    '.vscode',
+    '.expo-shared',
+    '.prettierrc',
+    'eslintrc.js'
   ],
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       typescript: {
         alwaysTryTypes: true,
-        project: "**/tsconfig.json",
-      },
-    },
-  },
-};
+        project: '**/tsconfig.json'
+      }
+    }
+  }
+}
